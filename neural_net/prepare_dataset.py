7397f0b3a118b4cb5333d847c877b828478cacef
MakowToms <makowskit2@student.mini.pw.edu.pl>
2021-03-04 14:01:48 +0100

    Feature: preparing dataset

---
+++ neural_net/prepare_dataset.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+
+def x_y_split(df, column):
+    dummies = pd.get_dummies(df[column], prefix=column)
+    df_without_column = df.drop([column], axis=1)
+    return np.array(df_without_column), np.array(dummies)
