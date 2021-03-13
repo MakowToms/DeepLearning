60371f101bdd336d78ac24909d8312d42180b93c
Laura <erdaradun.gaztea@gmail.com>
2021-03-13 14:57:16 +0100

    cleaned main.py

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from neural_net.neural_net import NeuralNet, Layer
 from neural_net.optimizers import momentum
 from neural_net.regularizations import L1_regularization
 from neural_net.weights import uniform_init
-from neural_net.prepare_dataset import x_y_split
-
+from neural_net.prepare_dataset import x_y_split, x_y_split_by_index
 
 # Classification
 train = pd.read_csv('datasets/neural_net/classification/data.simple.train.100.csv')
@@ -15,20 +14,21 @@ x_train, y_train = x_y_split(train, 'cls')
 test = pd.read_csv('datasets/neural_net/classification/data.simple.test.100.csv')
 x_test, y_test = x_y_split(test, 'cls')
 
-nn = NeuralNet(2, uniform_init)\
+nn = NeuralNet(2, weight_init=uniform_init)\
     .add_layer(Layer(10, sigmoid))\
     .add_layer(Layer(15, sigmoid))\
     .add_layer(Layer(10, sigmoid))\
     .add_layer(Layer(2, sigmoid))\
     .set_optimizer(momentum.set_params({"coef": 0.05}))\
     .set_loss(hinge)
-nn.budget.set_epoch_limit(20).set_detection_limit(1.3)
+nn.budget.set_epoch_limit(50).set_detection_limit(1.3)
 nn.train(x_train, y_train, x_test, y_test, learning_rate=0.02, batch_size=8)
 
 # Regression
-train = pd.read_csv('datasets/neural_net/regression/data.cube.test.100.csv')
-x_train = train.iloc[:, :-1]
-y_train = train.iloc[:, -1:]
+train = pd.read_csv('datasets/neural_net/regression/data.cube.train.100.csv')
+x_train, y_train = x_y_split_by_index(train, -1)
+test = pd.read_csv('datasets/neural_net/regression/data.cube.test.100.csv')
+x_test, y_test = x_y_split_by_index(test, -1)
 
 nn = NeuralNet(1, weight_init=uniform_init)\
     .add_layer(Layer(10, sigmoid))\
@@ -39,18 +39,4 @@ nn = NeuralNet(1, weight_init=uniform_init)\
     .set_regularization(L1_regularization.set_params({"coef": 0.05}))\
     .set_loss(MAE)
 nn.budget.set_epoch_limit(10).set_detection_limit(1.3)
-nn.train(x_train, y_train, learning_rate=0.02, batch_size=32)
-
-# Classification
-train = pd.read_csv('datasets/neural_net/classification/data.simple.train.100.csv')
-x_train, y_train = x_y_split(train, 'cls')
-
-nn = NeuralNet(2, weight_init=uniform_init)\
-    .add_layer(Layer(10, sigmoid))\
-    .add_layer(Layer(15, sigmoid))\
-    .add_layer(Layer(10, sigmoid))\
-    .add_layer(Layer(2, sigmoid))\
-    .set_optimizer(momentum.set_params({"coef": 0.05}))\
-    .set_loss(hinge)
-nn.budget.set_epoch_limit(50).set_detection_limit(1.3)
-nn.train(x_train, y_train, learning_rate=0.02, batch_size=8)
+nn.train(x_train, y_train, x_test, y_test, learning_rate=0.02, batch_size=16)
--- neural_net/prepare_dataset.py
+++ neural_net/prepare_dataset.py
@@ -6,3 +6,7 @@ def x_y_split(df, column):
     dummies = pd.get_dummies(df[column], prefix=column)
     df_without_column = df.drop([column], axis=1)
     return np.array(df_without_column), np.array(dummies)
+
+
+def x_y_split_by_index(df, index):
+    return np.array(df.iloc[:, :index]), np.array(df.iloc[:, index:])
