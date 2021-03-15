d57c801a8587362e81cb98793c2a02264a79dd47
Laura <erdaradun.gaztea@gmail.com>
2021-03-15 22:47:03 +0100

    added smaller, mean + std only table

--- utils/latex.py
+++ utils/latex.py
@@ -10,6 +10,28 @@ def add_separator(f, condition):
 
 def save_activation_error_to_latex(activation_error, activations, metrics, filename="temp.txt", shrink=False):
     with open(filename, "w") as f:
+        f.write(r"\begin{tabularx}{\textwidth}{ |X|X|X| }")
+        f.write("\n\\hline\n")
+
+        f.write(r"Measure & Activation & Value \\")
+        f.write("\n\\hline\n")
+
+        for met_index, metric in enumerate(metrics):
+            f.write(r"\multirow{{{0}}}*{{{1}}} & ".format(len(activations), metric))
+            for act_index, activation in enumerate(activations):
+                if act_index != 0:
+                    f.write(r"& ")
+                f.write(r"{0} & ".format(activation.name))
+                mean = statistics.mean(activation_error[act_index][met_index])
+                std = statistics.stdev(activation_error[act_index][met_index])
+                f.write(r"{0:.3f} \pm {1:.3f} \\".format(mean, std))
+                f.write("\n")
+            f.write("\\hline\n")
+        f.write("\\end{tabularx}\n")
+
+
+def save_activation_error_to_latex_wide(activation_error, activations, metrics, filename="temp.txt", shrink=False):
+    with open(filename, "w") as f:
         f.write(r"\begin{tabularx}{\textwidth}{ |" + "X|" * len(activations) * len(metrics) + " }\n")
         f.write("\\hline\n")
 
