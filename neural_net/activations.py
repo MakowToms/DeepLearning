97a732262e07ac8918bf2a6a21ca3afe717f366b
MakowToms <makowskit2@student.mini.pw.edu.pl>
2021-03-15 14:14:47 +0100

    Add activations names

--- neural_net/activations.py
+++ neural_net/activations.py
@@ -2,10 +2,11 @@ import numpy as np
 
 
 class Activation:
-    def __init__(self, function, derivative):
+    def __init__(self, function, derivative, name):
         """Works mostly as a pair of function and its derivative, it simply looks better this way."""
         self.function = function
         self.derivative = derivative
+        self.name = name
 
     def get_function(self):
         """Used to avoid ambiguous constructions like activation.function(func_params)."""
@@ -18,21 +19,26 @@ class Activation:
 
 linear = Activation(
     lambda x, layer: x,
-    lambda x, layer: 1
+    lambda x, layer: 1,
+    'linear'
 )
 ReLU = Activation(
     lambda x, layer: max(x, 0),
-    lambda x, layer: 1 if x > 0 else 0
+    lambda x, layer: 1 if x > 0 else 0,
+    'ReLU'
 )
 sigmoid = Activation(
     lambda x, layer: (np.exp(-x) + 1) ** (-1),
-    lambda x, layer: (np.exp(-x) + 1) ** (-1) * (1 - (np.exp(-x) + 1) ** (-1))
+    lambda x, layer: (np.exp(-x) + 1) ** (-1) * (1 - (np.exp(-x) + 1) ** (-1)),
+    'sigmoid'
 )
 tanh = Activation(
     lambda x, layer: 1 - 2 / (np.exp(2 * x) + 1),
-    lambda x, layer: 4 * (1 / (np.exp(2 * x) + 1) - 1 / (np.exp(2 * x) + 1) ** 2)
+    lambda x, layer: 4 * (1 / (np.exp(2 * x) + 1) - 1 / (np.exp(2 * x) + 1) ** 2),
+    'tanh'
 )
 softmax = Activation(
     lambda x, layer: np.exp(x) / np.exp(layer).sum(0),
-    lambda x, layer: np.exp(x) / np.exp(layer).sum(0) * (1 - np.exp(x) / np.exp(layer).sum(0))
+    lambda x, layer: np.exp(x) / np.exp(layer).sum(0) * (1 - np.exp(x) / np.exp(layer).sum(0)),
+    'softmax'
 )
