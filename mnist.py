cf503da9883f1011c1cc3ff320803a00a7c41d16
Laura <erdaradun.gaztea@gmail.com>
2021-03-22 11:42:49 +0100

    example call added to mnist

--- mnist.py
+++ mnist.py
@@ -2,7 +2,13 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+from neural_net.activations import tanh, softmax, linear
+from neural_net.losses import hinge, MSE
+from neural_net.neural_net import NeuralNet, Layer
+from neural_net.optimizers import RMSProp
 from neural_net.prepare_dataset import x_y_split
+from neural_net.regularizations import L1_regularization
+from neural_net.weights import uniform_init
 
 train = pd.read_csv('datasets/neural_net/mnist/train.csv')
 x_test = np.array(pd.read_csv('datasets/neural_net/mnist/test.csv'))
@@ -10,3 +16,12 @@ x_train, y_train = x_y_split(train, 'label')
 x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, train_size=35000, random_state=123)
 
 np.random.seed(123)
+
+nn = NeuralNet(x_train.shape[1], weight_init=uniform_init, name="mnist", is_regression=False) \
+    .add_layer(Layer(40, tanh)) \
+    .add_layer(Layer(y_train.shape[1], softmax)) \
+    .set_optimizer(RMSProp.set_params({"coef": 0.9})) \
+    .set_regularization(L1_regularization.set_params({"coef": 0.0001})) \
+    .set_loss(hinge)
+nn.budget.set_epoch_limit(100).set_detection_limit(1.4)
+nn.fit(x_train, y_train, x_val, y_val, learning_rate=0.01, batch_size=128)
