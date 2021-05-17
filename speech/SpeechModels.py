fed9d1f4239631a264df199a3d5d8942f497e932
ErdaradunGaztea <32573962+ErdaradunGaztea@users.noreply.github.com>
2021-05-17 21:55:24 +0200

    tried creating an amalgamation of CNN and AttRNN

--- speech/SpeechModels.py
+++ speech/SpeechModels.py
@@ -190,3 +190,100 @@ def AttRNNSpeechModel(nCategories, samplingrate=16000,
     model = Model(inputs=[inputs], outputs=[output])
 
     return model
+
+
+def ConvAttRNNSpeechModel(nCategories, samplingrate=16000, inputLength=16000, rnn_func=L.LSTM,
+                          more_blocks=False, bigger_blocks=False, blocks_layers=[20, 40, 80, 160, 320]):
+    # simple LSTM
+    sr = samplingrate
+    iLen = inputLength
+
+    inputs = L.Input((inputLength,), name='input')
+
+    x = L.Reshape((1, -1))(inputs)
+
+    m = Melspectrogram(n_dft=1024, n_hop=128, input_shape=(1, iLen),
+                       padding='same', sr=sr, n_mels=80,
+                       fmin=40.0, fmax=sr / 2, power_melgram=1.0,
+                       return_decibel_melgram=True, trainable_fb=False,
+                       trainable_kernel=False,
+                       name='mel_stft')
+    m.trainable = False
+
+    x = m(x)
+
+    x = Normalization2D(int_axis=0, name='mel_stft_norm')(x)
+
+    # note that Melspectrogram puts the sequence in shape (batch_size, melDim, timeSteps, 1)
+    # we would rather have it the other way around for LSTMs
+
+    x = L.Permute((2, 1, 3))(x)
+
+    c1 = L.Conv2D(blocks_layers[0], (5, 1), activation='relu', padding='same')(x)
+    c1 = L.BatchNormalization()(c1)
+    p1 = L.MaxPooling2D((2, 1))(c1)
+    p1 = L.Dropout(0.2)(p1)
+
+    c2 = L.Conv2D(blocks_layers[1], (3, 3), activation='relu', padding='same')(p1)
+    c2 = L.BatchNormalization()(c2)
+    if bigger_blocks:
+        c2 = L.Conv2D(blocks_layers[1], (3, 3), activation='relu', padding='same')(c2)
+        c2 = L.BatchNormalization()(c2)
+    p2 = L.MaxPooling2D((2, 2))(c2)
+    p2 = L.Dropout(0.3)(p2)
+
+    c3 = L.Conv2D(blocks_layers[2], (3, 3), activation='relu', padding='same')(p2)
+    c3 = L.BatchNormalization()(c3)
+    if bigger_blocks:
+        c3 = L.Conv2D(blocks_layers[2], (3, 3), activation='relu', padding='same')(c3)
+        c3 = L.BatchNormalization()(c3)
+    p3 = L.MaxPooling2D((2, 2))(c3)
+#     p3 = L.Dropout(0.3)(p3)
+
+    if more_blocks:
+        p3 = L.Dropout(0.3)(p3)
+        c3 = L.Conv2D(blocks_layers[3], (3, 3), activation='relu', padding='same')(p3)
+        c3 = L.BatchNormalization()(c3)
+        if bigger_blocks:
+            c3 = L.Conv2D(blocks_layers[3], (3, 3), activation='relu', padding='same')(c3)
+            c3 = L.BatchNormalization()(c3)
+        p3 = L.MaxPooling2D((2, 2))(c3)
+
+    x = L.Flatten()(p3)
+
+#     x = L.Conv2D(10, (5, 1), activation='relu', padding='same')(x)
+#     x = L.BatchNormalization()(x)
+#     x = L.Dropout(0.2)(x)
+#     x = L.Conv2D(1, (5, 1), activation='relu', padding='same')(x)
+#     x = L.BatchNormalization()(x)
+#     x = L.Dropout(0.2)(x)
+
+#     # x = Reshape((125, 80)) (x)
+#     # keras.backend.squeeze(x, axis)
+#     x = L.Lambda(lambda q: K.squeeze(q, -1), name='squeeze_last_dim')(x)
+
+    x = L.Bidirectional(rnn_func(64, return_sequences=True))(x)  # [b_s, seq_len, vec_dim]
+    x = L.Dropout(0.2)(x)
+    x = L.Bidirectional(rnn_func(64, return_sequences=True))(x)  # [b_s, seq_len, vec_dim]
+    x = L.Dropout(0.2)(x)
+
+    xFirst = L.Lambda(lambda q: q[:, -1])(x)  # [b_s, vec_dim]
+    query = L.Dense(128)(xFirst)
+
+    # dot product attention
+    attScores = L.Dot(axes=[1, 2])([query, x])
+    attScores = L.Softmax(name='attSoftmax')(attScores)  # [b_s, seq_len]
+
+    # rescale sequence
+    attVector = L.Dot(axes=[1, 1])([attScores, x])  # [b_s, vec_dim]
+
+    x = L.Dense(64, activation='relu')(attVector)
+    x = L.Dropout(0.2)(x)
+    x = L.Dense(32)(x)
+    x = L.Dropout(0.2)(x)
+
+    output = L.Dense(nCategories, activation='softmax', name='output')(x)
+
+    model = Model(inputs=[inputs], outputs=[output])
+
+    return model
